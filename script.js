3b5db66601d391471880f9e13158aac088c12c31
Weryclison Nandi <weryclison.nandi@gmail.com>
2023-11-23 14:28:17 -0300

    att

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const articles = document.querySelectorAll(".surgir");
-if (window.innerWidth < 750) {
-  articles[0].classList.remove("surgir");
-  articles[0].classList.add("ativo");
-}
-function aparecerArticle() {
-  let alto = window.innerHeight * 0.7;
-  articles.forEach((item) => {
-    let topo = item.getBoundingClientRect().top - alto < 0;
+// const articles = document.querySelectorAll(".surgir");
+// if (window.innerWidth < 750) {
+//   articles[0].classList.remove("surgir");
+//   articles[0].classList.add("ativo");
+// }
+// function aparecerArticle() {
+//   let alto = window.innerHeight * 0.7;
+//   articles.forEach((item) => {
+//     let topo = item.getBoundingClientRect().top - alto < 0;
 
-    if (topo) item.classList.add("ativo");
-  });
-}
+//     if (topo) item.classList.add("ativo");
+//   });
+// }
 
-window.addEventListener("scroll", aparecerArticle);
+// window.addEventListener("scroll", aparecerArticle);
